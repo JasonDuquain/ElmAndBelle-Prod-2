68f7cb96e7159f874dc1381d1e40fbfec4892506
Jason Duquain <jduquain@gmail.com>
2019-07-22 23:52:59 -0400

    btt btn aghhhh

--- js/script.js
+++ js/script.js
@@ -130,7 +130,10 @@ let bttBtn = document.querySelector('.js-btt-btn');
 /*let heights = [docElement.scrollHeight, docBody.scrollHeight, docElement.offsetHeight, docBody.offsetHeight];
 let highestHeight = Math.max(...heights);*/
 
+
 let highestHeight = docElement.scrollHeight;
+
+/*
 
 document.addEventListener('scroll', (e) => {
     (docElement.scrollTop > (highestHeight / 8)) ? bttBtn.classList.add('active') : bttBtn.classList.remove('active');
@@ -141,6 +144,7 @@ bttBtn.addEventListener('click', (e) => {
     docElement.scrollTop = 0;
 });
 
+*/
 
 /*****  SUBSCRIBE INPUTS CHANGE ICON COLOR ON FOCUS  ******/
 let inputs = document.querySelectorAll('form input');
