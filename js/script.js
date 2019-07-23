72fed9b2424efc1e1d736d73b77649f65b71bc42
Jason Duquain <jduquain@gmail.com>
2019-07-22 23:54:51 -0400

    why

--- js/script.js
+++ js/script.js
@@ -133,7 +133,6 @@ let highestHeight = Math.max(...heights);*/
 
 let highestHeight = docElement.scrollHeight;
 
-/*
 
 document.addEventListener('scroll', (e) => {
     (docElement.scrollTop > (highestHeight / 8)) ? bttBtn.classList.add('active') : bttBtn.classList.remove('active');
@@ -144,7 +143,7 @@ bttBtn.addEventListener('click', (e) => {
     docElement.scrollTop = 0;
 });
 
-*/
+
 
 /*****  SUBSCRIBE INPUTS CHANGE ICON COLOR ON FOCUS  ******/
 let inputs = document.querySelectorAll('form input');
